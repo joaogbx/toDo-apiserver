3ca94fc3cfaca95c90bdc2ea9a0a826a77bd93a8
João Gabriel <joao.gabriel@appfacilita.com>
2025-10-21 18:25:04 -0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { TaskModule } from 'src/tasks/task_module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersModule } from 'src/users/users.module';
+import { LoggerMiddleware } from 'src/common/middleware/logger_middleware';
 
 @Module({
   imports: [TaskModule, PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
